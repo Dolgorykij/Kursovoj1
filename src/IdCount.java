bed1c72ff78c91942de115edd89b98436433fbc4
Dolgorykij <dolgo-rykij@mail.ru>
2024-06-04 02:18:51 +0300

    basic

---
+++ src/IdCount.java
@@ -0,0 +1,20 @@
+//public class IdCount {        долго пытался сделать id в отдельном классе, в итоге задумка не вышла
+    //private int id;
+
+    //public IdCount() {
+        //id = 0;
+    //}
+    //public int getId() {
+        //return id;
+    //}
+    //public void increment() {
+        //id ++;
+    //}
+
+   // @Override
+   // public String toString() {
+        //return "IdCount{" +
+                //"id=" + id +
+                //'}';
+    //}
+//}
